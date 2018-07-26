7483e263b50c9c2de9b7efd126ae102647f17c41
davepallot <dave.pallot@uwa.edu.au>
2018-07-26 14:46:17 +0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(name='pyvospace',
+      version='0.1.0',
+      packages=find_packages(),
+      install_requires=['aiohttp',
+                        'aiohttp_jinja2',
+                        'aiohttp_security',
+                        'aiohttp-session',
+                        'asyncpg',
+                        'cryptography',
+                        'aio_pika',
+                        'passlib',
+                        'uvloop',
+                        'aiofiles',
+                        'riprova',
+                        'lxml',
+                        'cchardet',
+                        'aiodns',
+                        'aiojobs'],
+      entry_points={'console_scripts': [
+          'posix_space = pyvospace.server.spaces.posix.space.__main__:main',
+          'posix_storage = pyvospace.server.spaces.posix.storage.__main__:main']
+      })
