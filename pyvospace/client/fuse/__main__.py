ccbb89ee92f11f44c3e6479abf84e1dffff720bd
davepallot <dave.pallot@uwa.edu.au>
2018-09-03 11:10:27 +0800

    Continue to rework FUSE implemention.

--- pyvospace/client/fuse/__main__.py
+++ pyvospace/client/fuse/__main__.py
@@ -1,60 +1,78 @@
 #!/usr/bin/env python
 
-
+import os
 import json
+import stat
 import errno
 import time
 import requests
-from http import client
+import argparse
 
 from pyvospace.core.model import *
 
+from http import client
 from fuse import FUSE, FuseOSError, Operations
 from requests.auth import HTTPBasicAuth
 from urllib.parse import urlparse
 
 
 class VOSpaceFS(Operations):
-    def __init__(self, host, port, username, password):
+    def __init__(self, host, port, username, password, mountpoint):
         self.host = host
         self.port = port
         self.conn = {}
+        self.mountpoint = mountpoint
         self.session = requests.session()
         url = f"http://{host}:{port}/login"
         with self.session.post(url, auth=HTTPBasicAuth(username, password), verify=True) as r:
             r.raise_for_status()
-
         cookie_str = self.session.cookies['PYVOSPACE_COOKIE']
         self.cookie_str = f'PYVOSPACE_COOKIE={cookie_str}'
 
     # Filesystem methods
-    # ==================
 
     def access(self, path, mode):
-        #print('access', path)
         return 0
 
     def chmod(self, path, mode):
-        #print('chmod', path)
-        raise FuseOSError(errno.EACCES)
+        pass
 
     def chown(self, path, uid, gid):
-        #print('chown', path)
-        raise FuseOSError(errno.EACCES)
+        pass
 
     def getattr(self, path, fh=None):
-        url = f'http://{self.host}:{self.port}/vospace/nodes/{path}'
-        with self.session.get(url, params={'detail': 'max'}) as r:
-            if r.status_code == 404:
-                raise FuseOSError(errno.ENOENT)
-            r.raise_for_status()
-            node = Node.fromstring(r.text)
+        try:
+            url = f'http://{self.host}:{self.port}/vospace/nodes/{path}'
+            with self.session.get(url, params={'detail': 'max'}) as r:
+                if r.status_code == 403:
+                    raise FuseOSError(errno.EACCES)
+                if r.status_code == 404:
+                    raise FuseOSError(errno.ENOENT)
+                r.raise_for_status()
+                node = Node.fromstring(r.text)
 
-        node_props = node.properties
-        attr = node_props.get('ivo://icrar.org/vospace/core#getattr')
-        if not attr:
-            raise FuseOSError(errno.ENOENT)
-        return json.loads(attr.value)
+            node_props = node.properties
+            length = node_props.get('ivo://ivoa.net/vospace/core#length')
+            ctime = node_props.get('ivo://ivoa.net/vospace/core#ctime')
+            mtime = node_props.get('ivo://ivoa.net/vospace/core#mtime')
+
+            mode = 0o755
+            link = 2
+            if node.node_type == NodeType.ContainerNode:
+                mode = mode | stat.S_IFDIR
+            else:
+                link = 0
+                mode = mode | stat.S_IFREG
+
+            a = {'st_atime': float(mtime.value), 'st_ctime': float(ctime.value),
+                 'st_gid': 0, 'st_mode': mode,
+                 'st_mtime': float(mtime.value), 'st_nlink': link,
+                 'st_size': int(length.value), 'st_uid': 0}
+            return a
+        except FuseOSError:
+            raise
+        except:
+            raise FuseOSError(errno.EIO)
 
     def readdir(self, path, fh):
         try:
@@ -66,7 +84,9 @@ class VOSpaceFS(Operations):
                 raise FuseOSError(errno.EACCES)
             for n in node.nodes:
                 yield os.path.basename(n.path)
-        except Exception as e:
+        except FuseOSError:
+            raise
+        except:
             raise FuseOSError(errno.EBUSY)
 
     def readlink(self, path):
@@ -76,76 +96,98 @@ class VOSpaceFS(Operations):
         raise FuseOSError(errno.EACCES)
 
     def rmdir(self, path):
-        url = f'http://{self.host}:{self.port}/vospace/nodes/{path}'
-        with self.session.delete(url) as r:
-            r.raise_for_status()
+        try:
+            url = f'http://{self.host}:{self.port}/vospace/nodes/{path}'
+            with self.session.delete(url) as r:
+                if r.status_code == 403:
+                    raise FuseOSError(errno.EACCES)
+                r.raise_for_status()
+        except FuseOSError:
+            raise
+        except:
+            raise FuseOSError(errno.EIO)
 
     def mkdir(self, path, mode):
-        #print('mkdir', path)
-        node = ContainerNode(path)
-        url = f'http://{self.host}:{self.port}/vospace/nodes/{node.path}'
-        with self.session.put(url, data=node.tostring()) as r:
-            r.raise_for_status()
+        try:
+            node = ContainerNode(path)
+            url = f'http://{self.host}:{self.port}/vospace/nodes/{node.path}'
+            with self.session.put(url, data=node.tostring()) as r:
+                if r.status_code == 403:
+                    raise FuseOSError(errno.EACCES)
+                r.raise_for_status()
+        except FuseOSError:
+            raise
+        except:
+            raise FuseOSError(errno.EIO)
 
     def statfs(self, path):
-        url = f'http://{self.host}:{self.port}/vospace/nodes/{path}'
-        with self.session.get(url, params={'detail': 'max'}) as r:
-            r.raise_for_status()
-            node = Node.fromstring(r.text)
+        try:
+            url = f'http://{self.host}:{self.port}/vospace/nodes/{path}'
+            with self.session.get(url, params={'detail': 'max'}) as r:
+                r.raise_for_status()
+                node = Node.fromstring(r.text)
 
-        node_props = node.properties
-        attr = node_props.get('ivo://icrar.org/vospace/core#statfs')
-        if not attr:
+            node_props = node.properties
+            attr = node_props.get('ivo://icrar.org/vospace/core#statfs')
+            if not attr:
+                raise FuseOSError(errno.EIO)
+            return json.loads(attr.value)
+        except FuseOSError:
+            raise
+        except:
             raise FuseOSError(errno.EIO)
-        return json.loads(attr.value)
 
     def unlink(self, path):
-        #print('unlink', path)
-        url = f'http://{self.host}:{self.port}/vospace/nodes/{path}'
-        with self.session.delete(url) as r:
-            r.raise_for_status()
+        try:
+            url = f'http://{self.host}:{self.port}/vospace/nodes/{path}'
+            with self.session.delete(url) as r:
+                if r.status_code == 403:
+                    raise FuseOSError(errno.EACCES)
+                r.raise_for_status()
+        except FuseOSError:
+            raise
+        except:
+            raise FuseOSError(errno.EIO)
 
     def symlink(self, name, target):
         raise FuseOSError(errno.EACCES)
 
     def rename(self, old, new):
-        new_name = new[:-len(os.path.basename(old))]
-        #print('rename', old, new_name, new)
-
-        mv = Move(Node(old), Node(new_name))
-        url = f'http://{self.host}:{self.port}/vospace/transfers'
-        with self.session.post(url, data=mv.tostring()) as r:
-            #print('TRANSFER', r.status_code)
-            r.raise_for_status()
-            job = UWSJob.fromstring(r.text)
-
-        url = f'http://{self.host}:{self.port}/vospace/transfers/{job.job_id}/phase'
-        with self.session.post(url, data='PHASE=RUN') as r:
-            #print('PHASE', r.status_code)
-            r.raise_for_status()
-
-        poll_until = ('COMPLETED', 'ERROR')
-        while True:
-            with self.session.get(url) as r:
+        try:
+            mv = Move(Node(old), Node(new))
+            url = f'http://{self.host}:{self.port}/vospace/transfers'
+            with self.session.post(url, data=mv.tostring()) as r:
                 r.raise_for_status()
-                result = r.text
-            #print('POLL', result)
-            if result in poll_until:
-                break
-            time.sleep(0.05)
+                job = UWSJob.fromstring(r.text)
+
+            url = f'http://{self.host}:{self.port}/vospace/transfers/{job.job_id}/phase'
+            with self.session.post(url, data='PHASE=RUN') as r:
+                r.raise_for_status()
+
+            poll_until = ('COMPLETED', 'ERROR')
+            while True:
+                with self.session.get(url) as r:
+                    r.raise_for_status()
+                    result = r.text
+                if result in poll_until:
+                    if result == 'ERROR':
+                        raise FuseOSError(errno.EACCES)
+                    break
+                time.sleep(0.05)
+        except FuseOSError:
+            raise
+        except:
+            raise FuseOSError(errno.EIO)
 
     def link(self, target, name):
         raise FuseOSError(errno.EACCES)
 
     def utimens(self, path, times=None):
-        raise FuseOSError(errno.EACCES)
+        pass
 
     # File methods
-    # ============
 
     def open(self, path, flags):
-        #print('open', path, flags)
-
         node = DataNode(path)
         if flags == os.O_RDONLY:
             transfer = PullFromSpace(node, [HTTPGet()])
@@ -157,9 +199,13 @@ class VOSpaceFS(Operations):
         try:
             url = f'http://{self.host}:{self.port}/vospace/synctrans'
             with self.session.post(url, data=transfer.tostring()) as resp:
+                if resp.status_code == 403:
+                    raise FuseOSError(errno.EACCES)
                 resp.raise_for_status()
                 response = Transfer.fromstring(resp.text)
                 url = response.protocols[0].endpoint.url
+        except FuseOSError:
+            raise
         except:
             raise FuseOSError(errno.EIO)
 
@@ -180,8 +226,14 @@ class VOSpaceFS(Operations):
             response = None
             if isinstance(transfer, PullFromSpace):
                 response = conn.getresponse()
+                if response.status == 403:
+                    raise FuseOSError(errno.EACCES)
                 if response.status != 200:
                     raise FuseOSError(errno.EIO)
+        except FuseOSError:
+            if conn:
+                conn.close()
+            raise
         except:
             if conn:
                 conn.close()
@@ -191,23 +243,22 @@ class VOSpaceFS(Operations):
         return conn.sock.fileno()
 
     def create(self, path, mode, fi=None):
-        #print('create', path, mode)
         return self.open(path, os.O_WRONLY)
 
     def read(self, path, length, offset, fh):
-        #print('read', path, fh)
         conn = self.conn.get(fh)
         return conn[2].read(length)
 
     def write(self, path, buf, offset, fh):
-        #print('write', path, fh)
         conn = self.conn.get(fh)
-        chunk = f'{len(buf):X}\r\n'.encode('ascii') + buf + b'\r\n'
-        conn[0].sock.sendall(chunk)
+        chunked = []
+        chunked.append(f'{len(buf):X}\r\n'.encode('ascii'))
+        chunked.append(buf)
+        chunked.append(b'\r\n')
+        conn[0].sock.sendall(b''.join(chunked))
         return len(buf)
 
     def release(self, path, fh):
-        #print('close')
         conn = self.conn.get(fh)
         if not conn:
             return
@@ -215,15 +266,22 @@ class VOSpaceFS(Operations):
             try:
                 conn[0].send(b'0\r\n\r\n')
                 conn[0].getresponse()
-            except:
+            except Exception as e:
                 pass
         del self.conn[fh]
         conn[0].close()
 
-
 def main():
-    FUSE(VOSpaceFS('localhost', 8080, 'test', 'test'), '/tmp/vospace', nothreads=True, foreground=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str)
+    parser.add_argument("--port", type=int)
+    parser.add_argument("--username", type=str)
+    parser.add_argument("--password", type=str)
+    parser.add_argument("--mountpoint", type=str)
+    args = parser.parse_args()
 
+    space = VOSpaceFS(args.host, args.port, args.username, args.password, args.mountpoint)
+    FUSE(space, args.mountpoint, nothreads=True, foreground=True)
 
 if __name__ == '__main__':
     main()
