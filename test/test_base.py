92a6a00b4738814dd81eb0e80df83f224dd35b13
davepallot <dave.pallot@uwa.edu.au>
2018-10-25 10:20:27 +0800

    Added use_ssl to Space configuration.

--- test/test_base.py
+++ test/test_base.py
@@ -65,7 +65,9 @@ class TestBase(unittest.TestCase):
                                'dsn': 'postgres://vos_user:vos_user@localhost:5435/vospace',
                                'parameters': '{}',
                                'secret_key': 'ZlmNyXdQgRhhrC2Wwy-gLZj7Wv6ZtoKH',
-                               'domain': 'localhost'}
+                               'domain': 'localhost',
+                               'use_ssl': 0
+                               }
 
             config['Storage'] = {'name': 'posix',
                                  'host': 'localhost',
