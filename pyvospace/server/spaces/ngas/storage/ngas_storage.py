5626100f4d0f9107d6495dbea0f156a13381d43f
drtpotter <tobympotter@gmail.com>
2019-01-31 15:10:11 +0800

    Latest

--- pyvospace/server/spaces/ngas/storage/ngas_storage.py
+++ pyvospace/server/spaces/ngas/storage/ngas_storage.py
@@ -25,11 +25,12 @@ import uuid
 import asyncio
 import aiofiles
 import aiohttp
+import numpy as np
 
 from aiohttp import web
 from aiohttp_security import setup as setup_security
 from aiohttp_security import SessionIdentityPolicy
-from aiohttp_session import setup as setup_session
+from aiohttp_session import setup as setup_session, get_session
 from aiohttp_session.cookie_storage import EncryptedCookieStorage
 from contextlib import suppress
 from concurrent.futures import ProcessPoolExecutor
@@ -50,6 +51,12 @@ class NGASStorageServer(HTTPSpaceStorageServer):
         # What is this for?
         self.secret_key = self.config['Space']['secret_key']
         self.domain = self.config['Space']['domain']
+
+        # Choose an NGAS server, initially at random,
+        # there is probably a more elegant implementation
+        self.ngas_server_strings=self.config['Space']['ngas_servers']
+        server_index=np.random.choice([n for n in range(0,len(self.ngas_servers))],1)
+        self.ngas_server_string=self.ngas_servers[server_index]
 
         # Do I need a root_dir, probably not.
         self.root_dir = self.parameters['root_dir']
@@ -95,104 +102,206 @@ class NGASStorageServer(HTTPSpaceStorageServer):
         return app
 
     async def download(self, job: StorageUWSJob, request: aiohttp.web.Request):
-        # Download files from the server
+        # Download files from the server, returns a Response
         root_dir = self.root_dir
         path_tree = job.transfer.target.path
-        if job.transfer.target.node_type == NodeType.ContainerNode:
-            # Checking if request wants a tar file
-            if job.transfer.view != View('ivo://ivoa.net/vospace/core#tar'):
-                return web.Response(status=400, text=f'Unsupported Container View. '
-                                                     f'View: {job.transfer.view}')
 
-            tar_file = f'{self.staging_dir}/{uuid.uuid4()}/{os.path.basename(path_tree)}.tar'
-            stage_path = f'{self.staging_dir}/{uuid.uuid4()}/{path_tree}'
-            real_path = f'{self.root_dir}/{path_tree}'
-            async with job.transaction(exclusive=False):
-                await copy(real_path, stage_path)
+        # Get the session of a request? Is this the best way to do it?
+        session = await get_session(request)
 
-            try:
-                # Tarring up the sources, I might need to ask the database for all
-                loop = asyncio.get_event_loop()
-                await loop.run_in_executor(self.process_executor, tar,
-                                           stage_path, tar_file, os.path.basename(path_tree))
-                return await send_file(request, os.path.basename(tar_file), tar_file)
-            finally:
-                with suppress(Exception):
-                    await asyncio.shield(rmtree(os.path.dirname(tar_file)))
-                with suppress(Exception):
-                    await asyncio.shield(rmtree(os.path.dirname(stage_path)))
-        else:
-            file_path = f'{root_dir}/{path_tree}'
-            return await send_file(request, os.path.basename(path_tree), file_path)
+        # Get the UUID on the transaction
+        space_id=job.transfer.target.space_id
+
+        # Filename to be used with the NGAS object store
+        base_name=os.path.basename(path_tree)
+        filename_ngas=base_name
+
+        # URL for retrieval from NGAS
+        url_ngas=self.ngas_server_string+"/RETRIEVE"
+
+        # Make up the filename for retrieval from NGAS
+        # How can I get the uuid from the database?
+        params={"file_id" : filename_ngas}
+
+        # I suspect this streams content from the NGAS request to the client
+        # In a non-blocking way, but I am not sure
+        try:
+            # Connect to NGAS
+            resp_ngas = await session.get(url_ngas, params=params)
+            assert(resp_ngas.status==200), "Couldn't find file on NGAS server"
+
+            # Otherwise create the client
+            resp_client=web.StreamResponse()
+
+            # Do we need to do anything here with headers?
+            resp_client.headers=resp_ngas.headers
+
+            # Prepare the connection
+            await resp_client.prepare(request)
+
+            # Read and write to buffers
+            async for chunk in resp_ngas.content.iter_chunked(io.DEFAULT_BUFFER_SIZE):
+                if chunk:
+                    await resp_client.write(chunk)
+
+            # Finish the stream
+            await resp_client.write_eof()
+            return(resp_client)
+
+        except Exception:
+            # Question, should I raise an exception or send a response to the client
+            raise
+            #return web.Response(status=400, text="Sorry, server failed to retrieve file {} from NGAS server".format(base_name))
+
+
+        # Handling connection errors?
+
+        # root_dir = self.root_dir
+        # path_tree = job.transfer.target.path
+        # if job.transfer.target.node_type == NodeType.ContainerNode:
+        #     # Checking if request wants a tar file
+        #     if job.transfer.view != View('ivo://ivoa.net/vospace/core#tar'):
+        #         return web.Response(status=400, text=f'Unsupported Container View. '
+        #                                              f'View: {job.transfer.view}')
+        #
+        #     tar_file = f'{self.staging_dir}/{uuid.uuid4()}/{os.path.basename(path_tree)}.tar'
+        #     stage_path = f'{self.staging_dir}/{uuid.uuid4()}/{path_tree}'
+        #     real_path = f'{self.root_dir}/{path_tree}'
+        #     async with job.transaction(exclusive=False):
+        #         await copy(real_path, stage_path)
+        #
+        #     try:
+        #         # Tarring up the sources, I might need to ask the database for all
+        #         loop = asyncio.get_event_loop()
+        #         await loop.run_in_executor(self.process_executor, tar,
+        #                                    stage_path, tar_file, os.path.basename(path_tree))
+        #         return await send_file(request, os.path.basename(tar_file), tar_file)
+        #     finally:
+        #         with suppress(Exception):
+        #             await asyncio.shield(rmtree(os.path.dirname(tar_file)))
+        #         with suppress(Exception):
+        #             await asyncio.shield(rmtree(os.path.dirname(stage_path)))
+        # else:
+        #     file_path = f'{root_dir}/{path_tree}'
+        #     return await send_file(request, os.path.basename(path_tree), file_path)
 
     async def upload(self, job: StorageUWSJob, request: aiohttp.web.Request):
-        # Upload files to the server
+        # Upload files to the NGAS server
+        reader=request.content
 
-        # Stream on the content
-        reader = request.content
+        # Get the path tree
         path_tree = job.transfer.target.path
-        # UUID is created here?
-        target_id = uuid.uuid4()
-        # Base name of the file
-        base_name = f'{target_id}_{os.path.basename(path_tree)}'
-        # File name without UUID?
-        real_file_name = f'{self.root_dir}/{path_tree}'
-        # File name with UUID
-        stage_file_name = f'{self.staging_dir}/{base_name}'
+
+        # Get the session of a request? Is this the best way to do it?
+        session = await get_session(request)
+
+        # Get the UUID for the transaction
+        space_id = job.transfer.target.space_id
+
+        # Make up the URL for the retrieval from NGAS
+        url_ngas = self.ngas_server_string + "/ARCHIVE"
+
+        # Create the filename that is to be used with the NGAS object store
+        base_name=os.path.basename(path_tree)
+        filename_ngas=base_name
+
+        # Make up the filename for upload to NGAS
+        params = {"filename": filename_ngas}
+
+        # Stream content from the user to NGAS in a streaming manner
         try:
-            # Read from buffer to temporary file
-            size = 0
-            async with aiofiles.open(stage_file_name, 'wb') as f:
-                while True:
-                    buffer = await reader.read(io.DEFAULT_BUFFER_SIZE)
-                    if not buffer:
-                        break
-                    await fuzz()
-                    await f.write(buffer)
-                    size += len(buffer)
+            resp_ngas= await session.post(url_ngas, params=params)
+            size=0
+            async for buffer in reader.iter_chunked(io.DEFAULT_BUFFER_SIZE):
+                if buffer:
+                    size+=len(buffer)
+                    await resp_ngas.write(buffer)
+            await resp_ngas.write_eof()
 
-            if job.transfer.target.node_type == NodeType.ContainerNode:
-                # Check if the upload is a tar file?
-                # What do I do about directory structures?
-                if job.transfer.view != View('ivo://ivoa.net/vospace/core#tar'):
-                    return web.Response(status=400, text=f'Unsupported Container View. '
-                                                         f'View: {job.transfer.view}')
-                extract_dir = f'{self.staging_dir}/{target_id}/{path_tree}/'
-                try:
+            assert(resp_ngas.status==200)
 
-                    loop = asyncio.get_event_loop()
-                    # Untarring process
-                    root_node = await loop.run_in_executor(self.process_executor,
-                                                           untar,
-                                                           stage_file_name,
-                                                           extract_dir,
-                                                           job.transfer.target,
-                                                           self.storage)
-                    async with job.transaction() as tr:
-                        node = tr.target
-                        node.size = size
-                        node.storage = self.storage
-                        node.nodes = root_node.nodes
-                        await asyncio.shield(node.save())
-                        await asyncio.shield(copy(extract_dir, real_file_name))
-                finally:
-                    with suppress(Exception):
-                        await asyncio.shield(rmtree(f'{self.staging_dir}/{target_id}'))
-            else:
-                async with job.transaction() as tr:
-                    node = tr.target # get the target node that is associated with the data
-                    node.size = size # set the size
-                    node.storage = self.storage # set the storage back end so it can be found
-                    await asyncio.shield(fuzz01(2))
-                    await asyncio.shield(node.save()) # save details to db
-                    await asyncio.shield(move(stage_file_name, real_file_name)) # move in single transaction
-
-            # What's this reponse?
+            # Let the client know the transaction was successful
             return web.Response(status=200)
+
+            # Inform the database of new data
+            async with job.transaction() as tr:
+                node = tr.target # get the target node that is associated with the data
+                node.size = size # set the size
+                node.storage = self.storage # set the storage back end so it can be found
+                await asyncio.shield(fuzz01(2))
+                await asyncio.shield(node.save()) # save details to db
+
         except (asyncio.CancelledError, Exception):
-            # Handle a cancellation
+            # Handle a cancellation error
             raise
-        finally:
-            # Remove stage file name no matter what
-            with suppress(Exception):
-                await asyncio.shield(remove(stage_file_name))
+
+
+        # # Stream on the content
+        # reader = request.content
+        # path_tree = job.transfer.target.path
+        # # this UUID is only used for temporary staging file
+        # target_id = uuid.uuid4()
+        # # Base name of the file
+        # base_name = f'{target_id}_{os.path.basename(path_tree)}'
+        # # File name without UUID?
+        # real_file_name = f'{self.root_dir}/{path_tree}'
+        # # File name with UUID
+        # stage_file_name = f'{self.staging_dir}/{base_name}'
+        # try:
+        #     # Read from buffer to temporary file
+        #     size = 0
+        #     async with aiofiles.open(stage_file_name, 'wb') as f:
+        #         while True:
+        #             buffer = await reader.read(io.DEFAULT_BUFFER_SIZE)
+        #             if not buffer:
+        #                 break
+        #             await fuzz()
+        #             await f.write(buffer)
+        #             size += len(buffer)
+        #
+        #     if job.transfer.target.node_type == NodeType.ContainerNode:
+        #         # Check if the upload is a tar file?
+        #         # What do I do about directory structures?
+        #         if job.transfer.view != View('ivo://ivoa.net/vospace/core#tar'):
+        #             return web.Response(status=400, text=f'Unsupported Container View. '
+        #                                                  f'View: {job.transfer.view}')
+        #         extract_dir = f'{self.staging_dir}/{target_id}/{path_tree}/'
+        #         try:
+        #
+        #             loop = asyncio.get_event_loop()
+        #             # Untarring process
+        #             root_node = await loop.run_in_executor(self.process_executor,
+        #                                                    untar,
+        #                                                    stage_file_name,
+        #                                                    extract_dir,
+        #                                                    job.transfer.target,
+        #                                                    self.storage)
+        #             async with job.transaction() as tr:
+        #                 node = tr.target
+        #                 node.size = size
+        #                 node.storage = self.storage
+        #                 node.nodes = root_node.nodes
+        #                 await asyncio.shield(node.save())
+        #                 await asyncio.shield(copy(extract_dir, real_file_name))
+        #         finally:
+        #             with suppress(Exception):
+        #                 await asyncio.shield(rmtree(f'{self.staging_dir}/{target_id}'))
+        #     else:
+        #         async with job.transaction() as tr:
+        #             node = tr.target # get the target node that is associated with the data
+        #             node.size = size # set the size
+        #             node.storage = self.storage # set the storage back end so it can be found
+        #             await asyncio.shield(fuzz01(2))
+        #             await asyncio.shield(node.save()) # save details to db
+        #             await asyncio.shield(move(stage_file_name, real_file_name)) # move in single transaction
+        #
+        #     # http OK?
+        #     return web.Response(status=200)
+        #
+        # except (asyncio.CancelledError, Exception):
+        #     # Handle a cancellation
+        #     raise
+        # finally:
+        #     # Remove stage file name no matter what
+        #     with suppress(Exception):
+        #         await asyncio.shield(remove(stage_file_name))
---
+++ test/test_posix/test_create.py
@@ -0,0 +1,267 @@
+#
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia, 2018
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+
+import unittest
+import xml.etree.ElementTree as ET
+
+from xml.etree.ElementTree import tostring
+
+from test.test_base import TestBase
+from pyvospace.core.model import *
+
+
+class TestCreate(TestBase):
+
+    def tearDown(self):
+        self.loop.run_until_complete(self.delete('http://localhost:8080/vospace/nodes/test1'))
+        self.loop.run_until_complete(self.delete('http://localhost:8080/vospace/nodes/datanode'))
+        super().tearDown()
+
+    def test_container_model(self):
+        root = ContainerNode('/test')
+        root.insert_node_into_tree(ContainerNode('/test/test1'))
+        root.insert_node_into_tree(ContainerNode('/test/test1/test3'))
+        with self.assertRaises(InvalidArgument):
+            root.insert_node_into_tree(DataNode('/test/test1/test3/'))
+        with self.assertRaises(InvalidArgument):
+            root.insert_node_into_tree(Node('/test/test1/test3/test3/test4'))
+        root.insert_node_into_tree(Node('/test/test1/'), True)
+        with self.assertRaises(InvalidArgument):
+            root.insert_node_into_tree(Node('/test/'), True)
+        self.assertEqual(root, ContainerNode('/test', nodes=[Node('/test/test1/')]))
+
+        # add to a node deeper in the tree
+        root1 = ContainerNode('/root1/test2/test3')
+        root1.insert_node_into_tree(ContainerNode('/root1/test2/test3/test4'))
+        nodes = [n for n in Node.walk(root1)]
+        self.assertEqual(len(nodes), 2)
+
+        # check root
+        root1 = ContainerNode('/')
+        root1.insert_node_into_tree(ContainerNode('///root1'))
+        nodes = [n for n in Node.walk(root1)]
+        self.assertEqual(len(nodes), 2)
+
+
+    def test_create_delete(self):
+        async def run():
+            node = ContainerNode('test1')
+            await self.create_node(node)
+
+            node = Node('/test1/zzz')
+            await self.create_node(node)
+
+            node = ContainerNode('/test1/test2')
+            await self.create_node(node)
+
+            node = ContainerNode('/test1/test2/anothercontainer')
+            await self.create_node(node)
+
+            node = DataNode('/test1/test2/anothercontainer/test.tar')
+            await self.create_node(node)
+
+            for i in range(100):
+                node = Node(f'/test1/{i}')
+                await self.create_node(node)
+
+        self.loop.run_until_complete(run())
+
+    def test_get_protocol(self):
+        async def run():
+            status, response = await self.get('http://localhost:8080/vospace/protocols', params=None)
+            self.assertEqual(200, status, msg=response)
+
+        self.loop.run_until_complete(run())
+
+    def test_get_views(self):
+        async def run():
+            status, response = await self.get('http://localhost:8080/vospace/views', params=None)
+            self.assertEqual(200, status, msg=response)
+
+        self.loop.run_until_complete(run())
+
+    def test_get_properties(self):
+        async def run():
+            properties = [Property('ivo://ivoa.net/vospace/core#title', "Hello1", False),
+                          Property('ivo://ivoa.net/vospace/core#description', "Hello2", False)]
+            node1 = ContainerNode('/test1', properties=properties)
+            await self.create_node(node1)
+
+            status, response = await self.get('http://localhost:8080/vospace/properties', params=None)
+            self.assertEqual(200, status, msg=response)
+
+        self.loop.run_until_complete(run())
+
+    def test_set_properties(self):
+        async def run():
+            properties = [Property('ivo://ivoa.net/vospace/core#title', "Hello1", False),
+                          Property('ivo://ivoa.net/vospace/core#description', "Hello2", False)]
+            node1 = ContainerNode('/test1', properties=properties)
+
+            await self.create_node(node1)
+
+            # Set properties
+            properties = [Property('ivo://ivoa.net/vospace/core#title', "NewTitle"),
+                          DeleteProperty('ivo://ivoa.net/vospace/core#description')]
+            node1 = ContainerNode('/test1', properties=properties)
+
+            # Node doesnt exist
+            await self.set_node_properties(Node('/test2'), expected_status=404)
+
+            await self.set_node_properties(node1)
+
+            params = {'detail': 'max'}
+            node = await self.get_node('test1', params)
+            node.remove_property('ivo://ivoa.net/vospace/core#length')
+            node.remove_property('ivo://ivoa.net/vospace/core#btime')
+            node.remove_property('ivo://ivoa.net/vospace/core#ctime')
+            node.remove_property('ivo://ivoa.net/vospace/core#mtime')
+            node.remove_property('ivo://icrar.org/vospace/core#statfs')
+            prop = [Property('ivo://ivoa.net/vospace/core#title', "NewTitle", False)]
+            orig_node = ContainerNode('/test1', properties=prop)
+            self.assertEqual(node, orig_node)
+
+        self.loop.run_until_complete(run())
+
+    def test_create_node_fail(self):
+        async def run():
+            # XML Parse Error
+            status, response = await self.put('http://localhost:8080/vospace/nodes/',
+                                              data='<test><test>')
+            self.assertEqual(500, status, msg=response)
+
+            # URI name in node does not match URL path
+            node = Node('data1')
+            status, response = await self.put('http://localhost:8080/vospace/nodes/data',
+                                              data=node.tostring())
+            self.assertEqual(400, status, msg=response)
+
+            # Invalid attribute
+            node = Node('data1')
+            root = ET.fromstring(node.tostring())
+            root.attrib.pop('uri')
+            xml = tostring(root)
+            status, response = await self.put('http://localhost:8080/vospace/nodes/data1',
+                                              data=xml)
+            self.assertEqual(400, status, msg=response)
+
+            # Invalid node type
+            node = Node('data1')
+            root = ET.fromstring(node.tostring())
+            root.attrib['{http://www.w3.org/2001/XMLSchema-instance}type'] = 'vos:unknown'
+            xml = tostring(root)
+            status, response = await self.put('http://localhost:8080/vospace/nodes/data1',
+                                              data=xml)
+            self.assertEqual(400, status, msg=response)
+
+        self.loop.run_until_complete(run())
+
+    def test_create_node(self):
+        async def run():
+            properties = [Property('ivo://ivoa.net/vospace/core#title', "Test", True),
+                          Property('ivo://ivoa.net/vospace/core#description', "Hello", True)]
+            node = ContainerNode('test1', properties=properties)
+            await self.create_node(node)
+
+            node1 = ContainerNode('/test1/test2')
+            await self.create_node(node1)
+
+            node2 = Node('/test1/data')
+            await self.create_node(node2)
+
+            node3 = DataNode('/datanode')
+            await self.create_node(node3)
+
+            data_node = await self.get_node('/datanode', params={'detail': 'max'})
+            self.assertGreater(len(data_node.provides), 0)
+            self.assertGreater(len(data_node.accepts), 0)
+
+            # Test failure cases of getNode
+            params = {'detail': 'max'}
+            await self.get_node('/', params, expected_status=200)
+
+            # Duplicate Node
+            await self.create_node(node1, expected_status=409)
+
+            # Link Node
+            link_node = LinkNode('/test1/test2/test3', 'http://google.com')
+            await self.create_node(link_node)
+
+            node = await self.get_node('/test1/test2/test3', params={'detail': 'max'})
+            self.assertEqual(node.target, link_node.target)
+
+            # Check that Link Node is in Path
+            node3 = Node('/test1/test2/test3/test4')
+            await self.create_node(node3, expected_status=400)
+
+            # Node not found
+            params = {'detail': 'max'}
+            await self.get_node('mynode', params, expected_status=404)
+
+            # Container Node not found
+            node_not_found = Node('/test1/test2/test10/test11')
+            await self.create_node(node_not_found, expected_status=404)
+
+            # Invalid parameters
+            params = {'detail': 'invalid'}
+            await self.get_node('test1', params, expected_status=400)
+
+            params = {'limit': -1}
+            await self.get_node('test1', params, expected_status=400)
+
+            params = {'limit': "string"}
+            await self.get_node('test1', params, expected_status=400)
+
+            # get only one child of the container node, other should not be there.
+            # Should be in alphabetical order
+            params = {'detail': 'max', 'limit': 1}
+            node = await self.get_node('test1', params)
+            node.remove_property('ivo://ivoa.net/vospace/core#length')
+            node.remove_property('ivo://ivoa.net/vospace/core#btime')
+            node.remove_property('ivo://ivoa.net/vospace/core#ctime')
+            node.remove_property('ivo://ivoa.net/vospace/core#mtime')
+            node.remove_property('ivo://icrar.org/vospace/core#statfs')
+            cmp_node = ContainerNode('/test1',
+                                     properties=properties,
+                                     nodes=[Node('/test1/data')])
+
+            self.assertEqual(node, cmp_node)
+
+            params = {'detail': 'min'}
+            node = await self.get_node('test1', params)
+
+            # detail = min should not have properties
+            cmp_node = ContainerNode('/test1',
+                                     properties=properties,
+                                     nodes=[Node('/test1/data'),
+                                            ContainerNode('/test1/test2')])
+            self.assertNotEqual(node, cmp_node)
+
+            cmp_node = ContainerNode('test1',
+                                     nodes=[Node('/test1/data'),
+                                            ContainerNode('/test1/test2')])
+
+            self.assertEqual(node, cmp_node)
+
+        self.loop.run_until_complete(run())
+
+if __name__ == '__main__':
+    unittest.main()
